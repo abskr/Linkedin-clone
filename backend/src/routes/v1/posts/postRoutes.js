f221e1b02add0290b182789c1fdfd9c42c767e70
Sean Knowles <s@seanknowles.dev>
2021-05-11 09:00:56 +0100

    basic login completed

--- backend/src/routes/v1/posts/postRoutes.js
+++ backend/src/routes/v1/posts/postRoutes.js
@@ -1,48 +1,50 @@
-import express from "express";
-import PostModel from "../../../database/mongoDB/models/PostModel.js";
-import { asyncHandler } from "../../../core/asyncHandler.js";
-import { NotFoundError, ForbiddenError } from "../../../core/apiErrors.js";
-import { authGuard } from "../../../guard/authGuard.js";
+import express from 'express'
+import PostModel from '../../../database/mongoDB/models/PostModel.js'
+import { asyncHandler } from '../../../core/asyncHandler.js'
+import { NotFoundError, ForbiddenError } from '../../../core/apiErrors.js'
+import { authGuard } from '../../../guard/authGuard.js'
 
-const router = express.Router();
+const router = express.Router()
 
 // @route  GET v1/posts
 // @desc   Test route
 // @access Public
 router.get(
-  "/",
+  '/',
   asyncHandler(async (req, res, next) => {
-    const posts = await PostModel.find().populate('user', ['username', 'avatar']);
-    if (!posts) return next(new NotFoundError("No post yet!"));
+    const posts = await PostModel.find().populate('user', [
+      'username',
+      'avatar',
+    ])
+    if (!posts) return next(new NotFoundError('No post yet!'))
     console.log(PostModel)
-    res.status(200).send(posts);
+    res.status(200).send(posts)
   })
-);
+)
 
 // @route  POST v1/posts
 // @desc   Test route
 // @access Private
 router.post(
-  "/",
+  '/',
   authGuard,
   asyncHandler(async (req, res, next) => {
     const newPost = new PostModel(req.body)
     newPost.user = req.user.id
-    const {_id} = await newPost.save()
-    res.status(200).send(newPost);
+    const { _id } = await newPost.save()
+    res.status(200).send(newPost)
   })
-);
+)
 
 // @route  PUT v1/posts/:id
 // @desc   Test route
 // @access Private
 router.put(
-  "/:id",
+  '/:id',
   authGuard,
   asyncHandler(async (req, res, next) => {
-    
-    const {id} = req.user
-    
+    const { id } = req.user
+
     const post = await PostModel.findById(req.params.id)
 
     if (!post) {
@@ -51,20 +53,24 @@ router.put(
       }
       return next(new NotFoundError('Post not found!'))
     }
-    
-    const updatedPost = await PostModel.findByIdAndUpdate(req.params.id, req.body, {
-      runValidators: true,
-      new: true
-      })
+
+    const updatedPost = await PostModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
     res.status(200).send(updatedPost)
   })
-);
+)
 
 // @route  DELETE v1/posts/:id
 // @desc   Test route
 // @access Private
 router.delete(
-  "/:id",
+  '/:id',
   authGuard,
   asyncHandler(async (req, res, next) => {
     const post = await PostModel.findById(req.params.id)
@@ -77,15 +83,15 @@ router.delete(
     }
 
     const removedPost = await PostModel.findByIdAndDelete(req.params.id)
-    res.status(200).send('deleted!');
+    res.status(200).send('deleted!')
   })
-);
+)
 
 // @route  GET v1/posts/:id
 // @desc   Test route
 // @access Public
 router.get(
-  "/:id",
+  '/:id',
   authGuard,
   asyncHandler(async (req, res, next) => {
     const post = await PostModel.findById(req.params.id)
@@ -96,10 +102,9 @@ router.get(
       }
       return next(new NotFoundError('Post not found!'))
     }
-    res.status(200).send(post);
+    res.status(200).send(post)
   })
-);
-
+)
 
 
 export default router;
