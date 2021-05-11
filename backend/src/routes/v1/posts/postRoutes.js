39f7e1a489a6d5e789f1cd29135f4ba0b7c8b295
ABskr <bskr26@gmx.de>
2021-05-11 15:07:23 +0200

    import validator

--- backend/src/routes/v1/posts/postRoutes.js
+++ backend/src/routes/v1/posts/postRoutes.js
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
 import express from "express";
 import PostModel from "../../../database/mongoDB/models/PostModel.js";
+import { checkSchema } from 'express-validator'
+import { postValidator } from './postValidator.js'
+import { validationHandler } from '../../../core/validationHandler.js'
 import { asyncHandler } from "../../../core/asyncHandler.js";
 import { NotFoundError, ForbiddenError } from "../../../core/apiErrors.js";
 import { authGuard } from "../../../guard/authGuard.js";
 import cloudinary from "../../../services/image/cloudinaryUpload.js"
 import CloudinaryStorage from "multer-storage-cloudinary"
 import multer from "multer"
-=======
-import express from 'express'
-import PostModel from '../../../database/mongoDB/models/PostModel.js'
-import { asyncHandler } from '../../../core/asyncHandler.js'
-import { NotFoundError, ForbiddenError } from '../../../core/apiErrors.js'
-import { authGuard } from '../../../guard/authGuard.js'
->>>>>>> develop
 
 const router = express.Router()
 
@@ -39,7 +34,7 @@ router.get(
       'avatar',
     ])
     if (!posts) return next(new NotFoundError('No post yet!'))
-    console.log(PostModel)
+    //console.log(PostModel)
     res.status(200).send(posts)
   })
 )
@@ -49,7 +44,7 @@ router.get(
 // @access Private
 router.post(
   '/',
-  authGuard,
+  [authGuard, checkSchema(postValidator)],
   asyncHandler(async (req, res, next) => {
     validationHandler(req)
 
